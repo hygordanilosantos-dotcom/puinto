7964e976bda2bf68f97dcbb6324fbc8cc227cd00
flebt_yY <hygor.danilo.santos@escola.pr.gov.br>
2026-03-18 11:07:07 -0300

    Add files via upload

---
+++ puinto-main/pinto/grud.js
@@ -0,0 +1,21 @@
+let nome = ["carlos","Eduardo","Jorge","Ronaldo"]
+
+let numeros =[1,2,3,4];
+
+for(let i=0; i<10; i++){
+
+}
+let matriz=[
+    [20,30,40,50]
+    [10,45,77,28]
+    [22,53,84,92]
+
+];
+console.log(matriz)
+for(let i = 0; i<matriz.length; i++){{
+    for(let j = o 
+
+
+}
+
+}
